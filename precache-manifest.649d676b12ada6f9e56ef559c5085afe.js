3f3bbd3d2a460a8f6dc9d0803e3f016ed644a427
SamirZ <z.samir@live.com>
2019-04-22 21:37:36 +0200

    Updates

---
+++ precache-manifest.649d676b12ada6f9e56ef559c5085afe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/https://github.com/SamirZ/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "70fc97d34c149b62aab3",
+    "url": "/https://github.com/SamirZ/robofriends/static/js/runtime~main.5274e192.js"
+  },
+  {
+    "revision": "74f4714e786f4ff42d3e",
+    "url": "/https://github.com/SamirZ/robofriends/static/js/main.26da5410.chunk.js"
+  },
+  {
+    "revision": "63be156f5af5f8af8ac8",
+    "url": "/https://github.com/SamirZ/robofriends/static/js/2.e9580a22.chunk.js"
+  },
+  {
+    "revision": "74f4714e786f4ff42d3e",
+    "url": "/https://github.com/SamirZ/robofriends/static/css/main.d138bbbc.chunk.css"
+  },
+  {
+    "revision": "63be156f5af5f8af8ac8",
+    "url": "/https://github.com/SamirZ/robofriends/static/css/2.100c9f34.chunk.css"
+  },
+  {
+    "revision": "dcfcf09387e2c698282a94b5dc79b222",
+    "url": "/https://github.com/SamirZ/robofriends/index.html"
+  }
+];
