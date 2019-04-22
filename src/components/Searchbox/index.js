5a3f9ecea3c4df96c9629cd519d07c125c240932
SamirZ <z.samir@live.com>
2019-04-22 23:12:35 +0200

    Adding label to search box

--- src/components/Searchbox/index.js
+++ src/components/Searchbox/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const SearchBox = ({ searchChange }) => (
-  <div className='pa2'>
+  <div className='pa2 flex flex-column justify-center items-center'>
+    <label htmlFor='search' className='pa2 b lightest-blue'>
+      Search robots
+    </label>
     <input
+      id='search'
       onChange={searchChange}
-      className='pa3 ba b--green bg-lightest-blue'
+      className='pa3 ba b--green bg-lightest-blue mw5'
       type='search'
       placeholder='Search robots'
     />
